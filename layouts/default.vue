c45847f77024b32ef49232ddb61104918b94bbd9
AiMingQi <cafevalentine@gmail.com>
2018-09-23 07:30:51 -0600

    pug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,85 +1,60 @@
-<template>
-  <v-app dark>
-    <v-navigation-drawer
+<template lang="pug">
+  v-app(dark)
+    v-navigation-drawer(
       :mini-variant.sync="miniVariant"
       :clipped="clipped"
       v-model="drawer"
       fixed
       light
-      app
-    >
-      <v-list>
-        <v-list-tile
+      app)
+      h2 Top 5 Ways Crypto will Change the World
+      v-list
+        v-list-tile(
           router
           :to="item.to"
           :key="i"
           v-for="(item, i) in items"
-          exact
-        >
-          <v-list-tile-action>
-            <span>{{i+1}}</span>
-          </v-list-tile-action>
-          <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+          exact)
+          v-list-tile-action
+            span {{i+1}}
+          v-list-tile-title(v-text="item.title")
+    v-toolbar(fixed app :clipped-left="clipped")
+      v-toolbar-side-icon(@click="drawer = !drawer")
+      v-btn(
+      )
+        v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
+      v-btn(
+      )
+        v-icon web
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
+      v-btn(
         icon
         @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <nuxt-link to="/" class="white--text" style="text-decoration: none;">
-        <v-toolbar-title v-text="title"></v-toolbar-title>
-      </nuxt-link>
-      <v-spacer></v-spacer>
-      <v-btn
+      )
+        v-icon remove
+      nuxt-link(to="/" class="white--text" style="text-decoration: none;")
+        v-toolbar-title(v-text="title")
+      v-spacer
+      v-btn(
         icon
         @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
+      )
+        v-icon menu
+    v-content
+      v-container
+    v-navigation-drawer(
       temporary
       :right="right"
       v-model="rightDrawer"
       fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+    )
+      v-list
+        v-list-tile(@click.native="right = !right")
+          v-list-tile-action
+            v-icon(light) compare_arrows
+          v-list-tile-title Switch drawer (click me)
+    v-footer(:fixed="fixed" app)
+      span &copy; 2018 Crypto Catalyst
 </template>
 
 <script>
