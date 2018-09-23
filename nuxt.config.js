9fe5d196d9a6c772b5b26d697b5615a2ac14b304
AiMingQi <cafevalentine@gmail.com>
2018-09-23 07:18:01 -0600

    Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ccweb',
+    title: 'Crypto Catalyst',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
