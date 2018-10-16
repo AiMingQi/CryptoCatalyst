4ce3f570be4e9bd8d98906b45e5b9878fd8c187e
AiMingQi <cafevalentine@gmail.com>
2018-10-16 08:48:46 -0600

    start fresh?

--- pages/test.vue
+++ pages/test.vue
@@ -1,23 +1,13 @@
 <template lang="pug">
     div
-        label Name:
-        input(type="text" v-model="name")
-        v-btn Add
 </template>
 
 <script>
-import { namesRef } from './firebase';
-
 export default {
-    data () {
-        return {
-            name: 'Paul'
-        }
-    },
-    methods: {
-        submitName () {
-            namesRef.push({ name: this.name, edit: false })
-        }
+  data () {
+    return {
+      something: ''
     }
+  }
 }
 </script>
--- plugins/firebase.js
+++
@@ -1,8 +0,0 @@
-import { initilizeApp } from 'firebase'
-
-const app = initilizeApp ({
-    
-});
-
-export const db = app.database();
-export const namesRef = db.ref('names');
