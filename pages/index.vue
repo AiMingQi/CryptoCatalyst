84fff100a16658ae7ba03247c8206851920a11df
AiMingQi <cafevalentine@gmail.com>
2018-10-16 09:00:37 -0600

    cleanout

--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-  v-layout(column justify-center align-center)
-    v-flex(xs12 sm8 md6)
-      div.text-xs-center
-        v-img(src="/main-logo.png" alt="Crypto Catalyst Logo" class="mb-5")
-        h1 Where the Reaction is Taking Place
-</template>
--- plugins/vuetify.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-
-Vue.use(Vuetify, {
-  theme: {
-    primary: '#033E22',
-    secondary: '#033E22'
-  }
-})
