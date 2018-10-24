676c5ea84f2c83246be3365c50fc8ff0e495ccda
AiMingQi <cafevalentine@gmail.com>
2018-10-24 09:47:51 -0600

    heading change

--- dev/src/views/index.vue
+++ dev/src/views/index.vue
@@ -3,7 +3,7 @@
     v-flex(xs12 sm8 md6)
       div.pa-3.text-xs-center
         v-img(src="/main-logo.png" alt="Crypto Catalyst Logo" max-width="400px" class="mb-5 center" style="margin: 0 auto;")
-        h3 Our theory is that there are 5 types of ways that Crypto will change the world. 
+        h2 Our theory is that there are 5 types of ways that Crypto will change the world. 
         v-btn(to="/all-evidence" color="secondary") See the Evidence of the Reaction
         h2 This is a collection of the evidence of the reaction taking place.
 </template>
