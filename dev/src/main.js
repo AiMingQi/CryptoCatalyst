3225bf8fac152af14a5fa5604a61569660539609
AiMingQi <cafevalentine@gmail.com>
2018-10-25 09:28:10 -0600

    install CASL and initial setup

--- dev/src/main.js
+++ dev/src/main.js
@@ -3,12 +3,19 @@ import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ability from './ability'
+import { abilitiesPlugin } from '@casl/vue'
 
 import 'vuetify/dist/vuetify.min.css' 
 import './style/site.css'
 
 // index.js or main.js
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(abilitiesPlugin, ability)
+
+// TODO: the next line is added for debugging purposes and should be removed for production build
+window.ability = ability
 
 Vue.use(Vuetify, {
   theme: {
