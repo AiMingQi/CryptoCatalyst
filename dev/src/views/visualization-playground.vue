42f2a9067c7d0cb7ea125a2de248d28db4f1e114
AiMingQi <cafevalentine@gmail.com>
2018-11-11 09:07:56 -0700

    basic line visualization and attempt at sass-loader

--- dev/src/views/visualization-playground.vue
+++ dev/src/views/visualization-playground.vue
@@ -2,145 +2,53 @@
   div
     h1 Visualization Playground
     v-card
-      svg(@mousemove="mouseover" :width="width" :height="height")
-        g(:style="{transform: `translate(${margin.left}px, ${margin.top}px)`}")
-          path.aread(:d="paths.area")
-          path.line(:d="paths.line")
-          path.selector(:d="paths.selector")
+      <svg width="500" height="270">
+        <g style="transform: translate(0, 10px)">
+          <path :d="line" />
+        </g>
+      </svg>
 </template>
-
 <script>
-/* globals window, requestAnimationFrame */
 import * as d3 from 'd3';
-import TWEEN from 'tween.js';
-const props = {
-  data: {
-    type: Array,
-    default: () => [],
-  },
-  margin: {
-    type: Object,
-    default: () => ({
-      left: 0,
-      right: 0,
-      top: 10,
-      bottom: 10,
-    }),
-  },
-  ceil: {
-    type: Number,
-    default: 100,
-  },
-};
 export default {
-  name: 'area-chart',
-  props,
+  name: 'vue-line-chart',
   data() {
     return {
-      width: 0,
-      height: 0,
-      paths: {
-        area: '',
-        line: '',
-        selector: '',
-      },
-      lastHoverPoint: {},
-      scaled: {
-        x: null,
-        y: null,
-      },
-      animatedData: [],
-      points: [],
+      data: [99, 71, 78, 25, 36, 92],
+      line: '',
     };
   },
-  computed: {
-    padded() {
-      const width = this.width - this.margin.left - this.margin.right;
-      const height = this.height - this.margin.top - this.margin.bottom;
-      return { width, height };
-    },
-  },
   mounted() {
-    window.addEventListener('resize', this.onResize);
-    this.onResize();
-  },
-  beforeDestroy() {
-    window.removeEventListener('resize', this.onResize);
-  },
-  watch: {
-    data: function dataChanged(newData, oldData) {
-      this.tweenData(newData, oldData);
-    },
-    width: function widthChanged() {
-      this.initialize();
-      this.update();
-    },
+    this.calculatePath();
   },
   methods: {
-    onResize() {
-      this.width = this.$el.offsetWidth;
-      this.height = this.$el.offsetHeight;
+    getScales() {
+      const x = d3.scaleTime().range([0, 430]);
+      const y = d3.scaleLinear().range([210, 0]);
+      d3.axisLeft().scale(x);
+      d3.axisBottom().scale(y);
+      x.domain(d3.extent(this.data, (d, i) => i));
+      y.domain([0, d3.max(this.data, d => d)]);
+      return { x, y };
     },
-    createArea: d3.area().x(d => d.x).y0(d => d.max).y1(d => d.y),
-    createLine: d3.line().x(d => d.x).y(d => d.y),
-    createValueSelector: d3.area().x(d => d.x).y0(d => d.max).y1(0),
-    initialize() {
-      this.scaled.x = d3.scaleLinear().range([0, this.padded.width]);
-      this.scaled.y = d3.scaleLinear().range([this.padded.height, 0]);
-      d3.axisLeft().scale(this.scaled.x);
-      d3.axisBottom().scale(this.scaled.y);
-    },
-    tweenData(newData, oldData) {
-      const vm = this;
-      function animate(time) {
-        requestAnimationFrame(animate);
-        TWEEN.update(time);
-      }
-      new TWEEN.Tween(oldData)
-        .easing(TWEEN.Easing.Quadratic.Out)
-        .to(newData, 500)
-        .onUpdate(function onUpdate() {
-          vm.animatedData = this;
-          vm.update();
-        })
-        .start();
-      animate();
-    },
-    update() {
-      this.scaled.x.domain(d3.extent(this.data, (d, i) => i));
-      this.scaled.y.domain([0, this.ceil]);
-      this.points = [];
-      for (const [i, d] of this.animatedData.entries()) {
-        this.points.push({
-          x: this.scaled.x(i),
-          y: this.scaled.y(d),
-          max: this.height,
-        });
-      }
-      this.paths.area = this.createArea(this.points);
-      this.paths.line = this.createLine(this.points);
-    },
-    mouseover({ offsetX }) {
-      if (this.points.length > 0) {
-        const x = offsetX - this.margin.left;
-        const closestPoint = this.getClosestPoint(x);
-        if (this.lastHoverPoint.index !== closestPoint.index) {
-          const point = this.points[closestPoint.index];
-          this.paths.selector = this.createValueSelector([point]);
-          this.$emit('select', this.data[closestPoint.index]);
-          this.lastHoverPoint = closestPoint;
-        }
-      }
-    },
-    getClosestPoint(x) {
-      return this.points
-        .map((point, index) => ({ x:
-          point.x,
-          diff: Math.abs(point.x - x),
-          index,
-        }))
-        .reduce((memo, val) => (memo.diff < val.diff ? memo : val));
+    calculatePath() {
+      const scale = this.getScales();
+      const path = d3.line()
+        .x((d, i) => scale.x(i))
+        .y(d => scale.y(d));
+      this.line = path(this.data);
     },
   },
 };
 </script>
+
+<style scoped>
+svg {margin: 25px;}
+  
+path {
+  fill: none;
+  stroke: #76BF8A;
+  stroke-width: 3px;
+  }
+  
+</style>
