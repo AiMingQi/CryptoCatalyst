4392fd0fcd3bb4d32163cf5f59763d63b3e02a7a
AiMingQi <cafevalentine@gmail.com>
2018-11-22 10:04:19 -0700

    add content

--- dev/src/views/behind-the-curtain.vue
+++ dev/src/views/behind-the-curtain.vue
@@ -10,7 +10,8 @@
             h1 {{scene.title}}
           v-card-text
             div(v-html="scene.description")
-            v-btn.mt-5(:to="scene.link" block large) Visit Example
+            v-btn.mt-5(:to="scene.exampleLink" block large) Visit Example
+            v-btn(color="secondary" :href="scene.vendorLink" target="_blank" rel="noopener" block large) Our Vendor
 
 
 </template>
@@ -22,28 +23,33 @@ export default {
       scenes: [ 
         {
           title: "uPort Identity to Sign Our Distinguished Guest Book",
-          description: "<p>We are using uPort identity server to verify a  user's identity, ensure their uniqueness, allow that user to safely share their information with us, and verify that user upon their return. The record of the visit is currently being stored in Firebase, our Google based database. </p><p>In the future we will use the blockchain as a data source, but it is difficult to beat free*.  It would currently cost the gas price to record each article, which is only a few cents, but it ads up.  We are looking forward to doing an analysis on the actual cost of Google vs the cost of using the Ethereum Virtual Machine for example. This seems like a more useful comparison than ETH/USD.</p><p>* Turns out free doesn't scale very well and soon we will be forced to pay to use Google.  So we are looking forward to doing an analysis at where those crossover points are.</p>",
-          link: "/"
+          description: "<p>We are using uPort identity server to verify a user's identity, ensure their uniqueness, allow that user to safely share their information with us, and verify that user upon their return. The record of the visit is currently being stored in Firebase, our Google based database. </p><p>In the future we will use the blockchain as a data source, but it is difficult to beat free*.  It would currently cost the gas price to record each article, which is only a few cents, but it ads up.  We are looking forward to doing an analysis on the actual cost of Google vs the cost of using the Ethereum Virtual Machine for example. This seems like a more useful comparison than ETH/USD.</p><p>* Turns out free doesn't scale very well and soon we will be forced to pay to use Google.  So we are looking forward to doing an analysis at where those crossover points are.</p>",
+          exampleLink: "/",
+          vendorLink: "https://www.uport.me/"
         },
         {
           title: "uPort to Submit Time",
-          description: "",
-          link: "/time-submission"
+          description: "<p>We are using uPort identity server to verify a user's identity, ensure their uniqueness, allow that user to safely share their information with us, and verify that user upon their return. The record of the visit is currently being stored in Firebase, our Google based database. </p>",
+          exampleLink: "/time-submission",
+          vendorLink: "https://www.uport.me/"
         },
         {
           title: "AdEx for ads",
-          description: "",
-          link: "/"
+          description: "<p>AdEx is a decentralized exchange for ad units.  You can purchase ads directly from the advertiser.  Removing the middle man and in theory cutting out 40% of the cost.  You also get a more direct purchasing experience and protection against scams and bad advertising.</p>",
+          exampleLink: "/",
+          vendorLink: "https://www.adex.network/"
         },
         {
           title: "Aragon for governance",
-          description: "",
-          link: "/liquid-democracy"
+          description: "<p>It is amazing that Aragon has built such a comlete governance package of Ethereum 'Smart Contracts'. It is exciting to be able to actually use the technology to employ governance.  The Liquid Democracy package is still in development and when it is live we will be utilizing it for governance for certain.</p><p>If you are interested in participating in our governance than you are welcome.</p>",
+          exampleLink: "/liquid-democracy",
+          vendorLink: "https://mainnet.aragon.org/#/cryptocatalyst.aragonid.eth/"
         },
         {
           title: "Aragon for CCR token creation",
-          description: "",
-          link: "/liquid-democracy"
+          description: "<p>For every hour of work that you have committed to Crypto-Catalyst you will be rewarded* with 1 CCR** (Crypto-Catalyst-Reaction Token).</p><p>*Ownership and responsibility of control of the token once it is issued to you is completely up to you.</p><p>**The CCR (Crypto-Catalyst-Reaction) is an ERC20 token and requires a compatible ETH account to interact with it. MetaMask creates a web3 layer that will allow you to use the token to interact with our governance model at Aragon. If you receive the token at another Ethereum address you will probably not have the ability to use the token at Crypto-Catalyst at this time. If you would like to transfer the token to another party you will need to use a different type of wallet such as MyEtherWallet to complete the transfer. MetaMask doesn't support sending the token to other parties at this time.</p>",
+          exampleLink: "/liquid-democracy",
+          vendorLink: "https://mainnet.aragon.org/#/cryptocatalyst.aragonid.eth/"
         }
       ]
     }
